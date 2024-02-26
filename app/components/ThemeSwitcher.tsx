d88987ae2167abcf2db65cfd679418b5667c29d6
pankajk <pankaj@cloudtradetech.com>
2024-02-27 04:25:16 +0530

    add icons

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 
 
 export const ThemeSwitcher = () => {
@@ -23,7 +24,7 @@ export const ThemeSwitcher = () => {
             className={`w-fit absolute right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 bg-slate-200 dark:bg-[#212933]`}
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
-            {theme === "light" ? "Dark" : "Light"}
+            {theme === "light" ? <MoonIcon className="h-6 w-6 text-slate-500"/> : <SunIcon className="h-6 w-6 text-slate-400" />}
         </button>
     );
 };
